4670761951b031cf4c3736cb2f540f5f68b383e8
Tira Sundara <sundaralinus@gmail.com>
2023-03-31 16:14:49 +0700

    Defined followers and followees associations on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+  has_many :user_followees, foreign_key: :followee_id, class_name: "UserFollow"
+  has_many :user_followers, foreign_key: :follower_id, class_name: "UserFollow"
+
+  # list of followers or list of users who follows the user
+  has_many :followers, through: :user_followees, class_name: "User"
+
+  # list of users followed by the user
+  has_many :followees, through: :user_followers, class_name: "User"
+
   validates_presence_of :name
+
 end
