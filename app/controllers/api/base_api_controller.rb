650aa484fa4baa4779424f432ff57da8d5f70747
Tira Sundara <sundaralinus@gmail.com>
2023-04-04 08:20:20 +0700

    Implement BaseApiController

---
+++ app/controllers/api/base_api_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  class BaseApiController < ApplicationController
+
+    private
+
+    # Assume, the authentication system is already handled
+    # in real world, we can use token-based authentication such as JWT
+    def current_user
+      return @current_user if defined? @current_user
+
+      @current_user = User.find_or_create_by(name: "Bob")
+    end
+  end
+end
