003248ead0b2887cd4f90c8f682bab83c277a548
Tira Sundara <sundaralinus@gmail.com>
2023-04-05 23:26:55 +0700

    Define User#log_wake_up! to store user's wake up time data

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,12 @@ class User < ApplicationRecord
     new_sleep_record = sleep_records.build
     new_sleep_record.create_sleep_time!(sleep_ts: at)
   end
+
+  # User#log_wake_up! stores user's wake up time data
+  def log_wake_up!(at: nil)
+    at ||= Time.now.utc
+    new_wake_up_time = WakeUpTime.new(wake_up_ts: at)
+    sleep_records.update_or_create_by!(user: self, wake_up_time: new_wake_up_time)
+    new_wake_up_time
+  end
 end
