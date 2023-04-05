ffeb1c29a062674d5effd7be8f3e0a96b49c93d8
Tira Sundara <sundaralinus@gmail.com>
2023-04-06 01:08:05 +0700

    Refactor: introduced SleepRecordable module. Modularized the code

---
+++ app/models/concerns/sleep_recordable.rb
@@ -0,0 +1,22 @@
+module SleepRecordable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :sleep_records
+  end
+
+  # User#log_sleep! stores user's sleep clock-in data
+  def log_sleep!(at: nil)
+    at ||= Time.now.utc
+    new_sleep_record = sleep_records.build
+    new_sleep_record.create_sleep_time!(sleep_ts: at)
+  end
+
+  # User#log_wake_up! stores user's wake up time data
+  def log_wake_up!(at: nil)
+    at ||= Time.now.utc
+    new_wake_up_time = WakeUpTime.new(wake_up_ts: at)
+    sleep_records.update_or_create_by!(user: self, wake_up_time: new_wake_up_time)
+    new_wake_up_time
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,6 @@
 class User < ApplicationRecord
   include Followable
-
-  has_many :sleep_records
+  include SleepRecordable
 
   validates_presence_of :name
-
-  # User#log_sleep! stores user's sleep clock-in data
-  def log_sleep!(at: nil)
-    at ||= Time.now.utc
-    new_sleep_record = sleep_records.build
-    new_sleep_record.create_sleep_time!(sleep_ts: at)
-  end
-
-  # User#log_wake_up! stores user's wake up time data
-  def log_wake_up!(at: nil)
-    at ||= Time.now.utc
-    new_wake_up_time = WakeUpTime.new(wake_up_ts: at)
-    sleep_records.update_or_create_by!(user: self, wake_up_time: new_wake_up_time)
-    new_wake_up_time
-  end
 end
